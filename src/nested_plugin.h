d0c13fc3555b5e45f58aac9e5357ed3372d5fb0b
Francois Beutin <francois.beutin@ledger.fr>
2025-09-10 11:45:41 +0200

    Add support for APEX_P

--- src/nested_plugin.h
+++ src/nested_plugin.h
@@ -209,9 +209,9 @@ typedef struct context_t {
     uint8_t booleans;
 } context_t;
 
-// Piece of code that will check that the above structure is not bigger than 5
-// * 32. Do not remove this check.
-_Static_assert(sizeof(context_t) <= 5 * 32, "Structure of parameters too big.");
+// Check that the plugin context structure will fit in the ethereum allocated memory.
+// for us Do not remove!
+ASSERT_SIZEOF_PLUGIN_CONTEXT(context_t);
 
 #define copy_number(parameter, T)              \
     _Generic((T), uint32_t *                   \
